b88e8645e37735acb66318b798b23d835616108a
Shahin J. Karimov <shahinjk@code.local>
2026-04-19 14:29:49 +0400

    tut geldi

---
+++ PR/Model/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR.Model
+{
+    internal class Car
+    {
+        public int MyProperty { get; set; }
+    }
+}
