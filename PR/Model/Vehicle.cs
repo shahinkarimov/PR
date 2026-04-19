cd1c1ecad6bb1de8eb5a4d4ad858d5d5b195e0b1
akbarin <akbarin@code.edu.az>
2026-04-19 14:01:16 +0400

    last changes

---
+++ PR/Model/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR.Model
+{
+    internal class Vehicle
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+        public double Mile { get; set; }
+        public string MyProperty { get; set; }
+
+
+
+    }
+}
